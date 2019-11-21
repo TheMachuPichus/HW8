4109f6b7503c5f8fad1712870a042815b0537b31
Demo User <debian@beaglebone.localdomain>
2019-11-21 15:51:22 +0000

    Updated main

--- Problem1/temp-launcher.py
+++ Problem1/temp-launcher.py
@@ -2,11 +2,15 @@ import os
 import sys
 import time
 
-xname=sys.argv[1]
-i = 0
+def main():
+  xname=sys.argv[1]
+  i = 0
+  
+  while i < int(xname):
+    os.system("./build.out")  
+    i += 1
+    time.sleep(1)
 
-while i < int(xname):
-  os.system("./build.out")  
-  i += 1
-  time.sleep(1)
+if __name__=='__main__':
+  main()
 
